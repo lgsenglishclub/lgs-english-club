7ba6422f2db06ae5551e73e039e3120b22002808
LGS English Club <englishlgsclub@gmail.com>
2026-08-06 23:17:53 +0300

    Yeni özellik

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,7 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 const firebaseConfig = {
--- pages/admin-message.js
+++ pages/admin-message.js
@@ -1,4 +1,4 @@
-import { db } from "../firebase.js";
+import { auth, db } from "../firebase-config.js";
 
 let message = null;
 
